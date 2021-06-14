cadfce1ee72696efe87ebb15da0b6f94de85bd5b
VictorovIvan <victorov.i.vl@yandex.ru>
2021-06-14 04:35:14 +0300

    Added project

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: "development",
+
+    module: {
+        rules: [
+
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+
+            },
+
+            // Loading images
+            {
+                test: /\.(png|jpg|jpeg|gif|ico)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'img',
+                        name: '[name]-[sha1:hash:7].[ext]'
+                    }
+                }]
+            },
+
+            // Loading fonts
+            {
+                test: /\.(ttf|otf|eot|woff|woff2)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'fonts',
+                        name: '[name].[ext]'
+                    }
+                }]
+            },
+            // Loading css
+            {
+                test: /\.(css)$/,
+                use: [ MiniCssExtractPlugin.loader , 'css-loader']
+            },
+            // Loading scss
+            {
+                test: /\.(s[ca]ss)$/,
+                use: [ MiniCssExtractPlugin.loader , 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Hello World',
+            buildTime: new Date().toISOString(),
+            template: 'public/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'main-[hash:8].css'
+        })
+    ]
+};
